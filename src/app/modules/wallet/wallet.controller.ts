db22b942942fd422e321aa13d1028e97e0c8be56
sabbirziauddin <sabbircseiiuc@gmail.com>
2025-09-04 15:18:12 +0200

    vercel configured

--- src/app/modules/wallet/wallet.controller.ts
+++ src/app/modules/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { walController } from './wallet.controller';
+
 import httpStatus from 'http-status-codes';
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsynch";
